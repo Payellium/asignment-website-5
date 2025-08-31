08f06a225dabf4ff9276cd58b99a35df2fa9034c
Payel Islam <payelstein85@gmail.com>
2025-08-31 20:49:01 +0600

    made the website resposive for smaller device

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+let count = 0;
+document.getElementById("heart-icon-1").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-2").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-3").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-4").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-5").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-6").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-7").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-8").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+document.getElementById("heart-icon-9").addEventListener("click", function(){
+    
+    const heartCount = document.getElementById("heart-count")
+    count++;
+    heartCount.innerText = count;
+})
+
+
+
